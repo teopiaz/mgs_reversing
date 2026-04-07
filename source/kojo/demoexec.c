5959eb9646197a96355b8ad53fac95f591c3131d
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-04-07 13:32:28 +0200

    Port: Update PORT_BUILD preprocessor directive to PORT_BUILD_VERBOSE for enhanced logging

--- source/kojo/demoexec.c
+++ source/kojo/demoexec.c
@@ -603,7 +603,7 @@ static BOOL ShowEffect(LPMGSDEMOACT lpAct, DMO_DATA_0x36 *data, ACTNODE *node)
     DG_SetPos2(&svec1, &svec2);
     ReadRotMatrix(&mat2);
 
-#ifdef PORT_BUILD
+#ifdef PORT_BUILD_VERBOSE
     {
         static int _mc = 0;
         if (_mc < 20) {
